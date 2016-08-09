dc20d54c957d1d80a94c615450d37bf287426e4d
Giymo11 <gizmo.head@yahoo.de>
2016-08-09 13:41:38 +0200

    `!roll`: *enhance*

--- bot/src/main/scala/rip/hansolo/discord/tini/ShitTiniSays.scala
+++ bot/src/main/scala/rip/hansolo/discord/tini/ShitTiniSays.scala
@@ -32,6 +32,7 @@ object ShitTiniSays {
                |!catfacts - I will show you some catfacts. :cat2:
                |!shutup - I will not respond to every message anymore.
                |!8ballmode - I will start responding again. Default mode :stuck_out_tongue_winking_eye:
+               |!roll <lower-bound> <upper-bound> - Returns a number between those numbers
              """.stripMargin
 
   private[this] val catfacts = {
@@ -45,13 +46,40 @@ object ShitTiniSays {
   private[this] def rollMessage = Util.oneOf("You rolled: ", ":game_die: says: ", "RNGesus says: ", "My Quantum Random Number Generator says: ", "")
   def rollTheDice(command: String): String = {
     val args = command.trim.split(" ")
-    val strings = if(args.size == 3) Xor.Right(args(1), args(2)) else Xor.Left(new IllegalArgumentException("Should have exactly two parameters"))
-    val ints = strings.flatMap { case (from, to) => Xor.catchNonFatal((from.toInt, to.toInt)) }
-    val result = ints.flatMap { case (from, to) => Xor.catchNonFatal(Random.nextInt(to - from) + from) }
-    result match {
-      case Xor.Right(value) => rollMessage + s"**$value**"
-      case Xor.Left(_) => "Usage: `!roll <lower-bound> <upper-bound>`"
+    val message = Xor.catchNonFatal {
+      if(args.size == 2) {  // !roll 2d6
+        val dice = args(1)
+        if(dice.contains("d")) {
+          val parts = dice.split("d")
+          val results =
+            if(parts.size == 2) { // 2, 6
+              val count = parts(0).toInt
+              val sides = parts(1).toInt
+              if(count <= 0 || sides <= 0) throw new IllegalArgumentException("I need ma counts")
+              for(i <- 1 to count) yield Random.nextInt(sides) + 1
+            } else Seq()
+          if(results.size == 1) results.head.toString
+          else s"( ${results.mkString(" + ")} ) = **${results.sum}**"
+        } else {
+          val upper = args(1).toInt
+          s"**${Util.oneOf(1 to upper: _*)}**"
+        }
+      } else if (args.size == 3) { // !roll 2 20
+      val args = command.trim.split(" ")
+        val lower = args(1).toInt
+        val upper = args(2).toInt
+        s"**${Util.oneOf(lower to upper: _*)}**"
+      } else throw new IllegalArgumentException("Needs better arguments!")
+    }
+
+    message match {
+      case Xor.Right(value) => rollMessage + value
+      case Xor.Left(_) =>
+        """
+          |Usage:
+          |`!roll <lower-bound> <upper-bound>`, example: `!roll 1 10`
+          |`!roll <count>d<sides>`, example: `!roll 2d6`
+        """.stripMargin
     }
   }
-
 }
