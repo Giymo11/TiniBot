33b80fa3c936af07f8dea3eb90f848c8de61f507
Raffy23 <raphael.ludwig@happywetter.at>
2016-08-22 21:45:34 +0200

    !repeat command has now a minmal duration of 15 minutes to prevent spamming Added a !say command !shutup won't kill !repeat commands of other channels

--- bot/src/main/scala/rip/hansolo/discord/tini/commands/Repeat.scala
+++ bot/src/main/scala/rip/hansolo/discord/tini/commands/Repeat.scala
@@ -7,6 +7,7 @@ import monix.execution.atomic.Atomic
 import net.dv8tion.jda.entities.Message
 import rip.hansolo.discord.tini.brain.TextBrainRegion
 
+import scala.collection.concurrent.TrieMap
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.duration._
 
@@ -19,7 +20,9 @@ import scala.concurrent.duration._
 object Repeat extends Command {
 
   override def prefix: String = "repeat"
-  private val repeatTasks = new ListBuffer[CancelableFuture[Unit]]
+
+  private val repeatTasks = new TrieMap[String,ListBuffer[CancelableFuture[Unit]]]()
+  private val defaultDuration = 15
 
   /**
     * @param args    The return of its unapply. It's the String needed for the execution of the command
@@ -29,10 +32,9 @@ object Repeat extends Command {
   override def exec(args: String, message: Message): Unit = {
     val arguments = args.split(" ")
 
-    //TODO: make it pretty
     if( arguments.length >= 2 ) {
       val count = Atomic(arguments.head.toInt - 1)
-      val duration = Xor.catchNonFatal(if (arguments(1).toInt > 10) arguments(1).toInt else 10 ).toOption
+      val duration = Xor.catchNonFatal(if (arguments(1).toInt > defaultDuration) arguments(1).toInt else defaultDuration ).toOption
 
       val cmdStart = if( duration.isEmpty ) 1 else 2
 
@@ -41,20 +43,28 @@ object Repeat extends Command {
         cmd.getOrElse(NotACommand).exec(arguments.drop(cmdStart+1).mkString(" "), message)
       }
 
-      message.getChannel.sendMessageAsync(s":ok_hand: Tini will repeat `${arguments(cmdStart)}` every ***${duration.getOrElse(10) seconds}***",null)
+      message.getChannel.sendMessageAsync(s"Tini will repeat `${arguments.drop(cmdStart).mkString(" ")}` " +
+        s"***${count.get}*** times and every ***${duration.getOrElse(defaultDuration) minutes}***",null)
 
       repTask.runAsync
-      repeatTasks += repTask.delayExecution(duration.getOrElse(10) seconds)
-                            .restartUntil((Unit) => { count.getAndDecrement(1) == 1 } ) // because this gets the value before updating it
-                            .runAsync
+      val tasks  = repeatTasks.getOrElseUpdate(message.getChannelId,new ListBuffer[CancelableFuture[Unit]])
+      val future = repTask.delayExecution(duration.getOrElse(defaultDuration) minutes)
+                          .restartUntil((Unit) => { count.getAndDecrement(1) == 1 } ) // because this gets the value before updating it
+                          .runAsync
 
+      future andThen { case _ => repeatTasks(message.getChannelId) -= future } // can't use onFinish from Task cuz variable is not fully set
+      tasks += future
     } else {
       message.getChannel.sendMessageAsync("Usage: " + longHelp, null)
     }
 
   }
 
-  def shutup() = repeatTasks foreach { _.cancel() }
+  def stopRepeat(channelID: String): Unit = repeatTasks.get(channelID) match {
+    case Some(channel) => channel foreach { _.cancel() }
+    case _ =>
+  }
+  def shutup() = repeatTasks foreach { _._2 foreach { _.cancel() } }
 
   override def longHelp: String = s"`$command <count> [timeout] <command> <command-args>` - Repeat the command multiple times with the arguments"
   override def shortHelp: String =  s"`$command` - the bot will repeat some messages"
--- bot/src/main/scala/rip/hansolo/discord/tini/commands/Say.scala
+++ bot/src/main/scala/rip/hansolo/discord/tini/commands/Say.scala
@@ -1,16 +1,15 @@
 package rip.hansolo.discord.tini.commands
 import net.dv8tion.jda.entities.Message
-import rip.hansolo.discord.tini.brain.TiniBrain
-import rip.hansolo.discord.tini.resources.ShitTiniSays
 
 /**
   * Created by: 
   *
   * @author Raphael
-  * @version 17.08.2016
+  * @version 22.08.2016
   */
-object Shutup extends Command {
-  override def prefix: String = "shutup"
+object Say extends Command {
+
+  override def prefix: String = "say"
 
   /**
     * @param args    The return of its unapply. It's the String needed for the execution of the command
@@ -18,12 +17,9 @@ object Shutup extends Command {
     * @param message The message which
     */
   override def exec(args: String, message: Message): Unit = {
-    TiniBrain.is8ball.set(false)
-    Repeat.stopRepeat(message.getChannelId)
-
-    message.getChannel.sendMessageAsync(ShitTiniSays.shutupResponse,null)
+    message.getChannel.sendMessageAsync(args,null)
   }
 
   override def longHelp: String = shortHelp
-  override def shortHelp: String = s"`$command` - not do that anymore."
+  override def shortHelp: String = s"`$command <text>` - Tini will say that"
 }
