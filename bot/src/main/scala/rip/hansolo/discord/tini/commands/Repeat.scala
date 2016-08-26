b43ef75b04da3db464d96d57362b622e2ff59855
Giymo11 <gizmo.head@yahoo.de>
2016-08-26 03:03:27 +0200

    Enables whitespace in between the Tini Prefix and the Command Prefix Also, adds support for line breaks in between the repeat command and the arguments.

--- bot/src/main/scala/rip/hansolo/discord/tini/commands/Repeat.scala
+++ bot/src/main/scala/rip/hansolo/discord/tini/commands/Repeat.scala
@@ -1,15 +1,23 @@
 package rip.hansolo.discord.tini.commands
+
+
+import scala.collection.concurrent.TrieMap
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration._
+
 import cats.data.Xor
+
 import monix.eval.Task
 import monix.execution.CancelableFuture
 import monix.execution.Scheduler.Implicits.global
 import monix.execution.atomic.Atomic
 import net.dv8tion.jda.entities.Message
-import rip.hansolo.discord.tini.brain.TextBrainRegion
 
-import scala.collection.concurrent.TrieMap
-import scala.collection.mutable.ListBuffer
-import scala.concurrent.duration._
+import rip.hansolo.discord.tini.Util._
+import rip.hansolo.discord.tini.brain.{TextBrainRegion, TiniBrain}
+
+import scala.language.postfixOps
+
 
 /**
   * Created by: 
@@ -21,8 +29,10 @@ object Repeat extends Command {
 
   override def prefix: String = "repeat"
 
-  private val repeatTasks = new TrieMap[String,ListBuffer[CancelableFuture[Unit]]]()
-  private val defaultDuration = 15
+  private val repeatTasks = new TrieMap[String, ListBuffer[CancelableFuture[Unit]]]()
+  private val minimumDuration = 15
+
+  def charsToDrop = TiniBrain.tiniPrefix.get.length
 
   /**
     * @param args    The return of its unapply. It's the String needed for the execution of the command
@@ -30,34 +40,45 @@ object Repeat extends Command {
     * @param message The message which
     */
   override def exec(args: String, message: Message): Unit = {
-    val arguments = args.split(" ")
+    val arguments = args.dropWhile(isWhitespace).split(" ")
+    println("Prefix: " + arguments.head)
 
     if( arguments.length >= 2 ) {
       val count = Atomic(arguments.head.toInt - 1)
-      val duration = Xor.catchNonFatal(if (arguments(1).toInt > defaultDuration) arguments(1).toInt else defaultDuration ).toOption
+      var isRescheduled = false
+      val duration = Xor.catchNonFatal(
+        if (arguments(1).toInt > minimumDuration)
+          arguments(1).toInt
+        else {
+          isRescheduled = true
+          minimumDuration
+        }).toOption
 
       val cmdStart = if( duration.isEmpty ) 1 else 2
 
+      val newArgs = args.dropWhile(isWhitespace)
+        .split(" ").drop(cmdStart).mkString(" ")
+        .dropWhile(isWhitespace)
+        .split(" ")
+
       val repTask = Task {
-        val cmd = TextBrainRegion.channelCommands.get(arguments(cmdStart).drop(1))
-        cmd.getOrElse(NotACommand).exec(arguments.drop(cmdStart+1).mkString(" "), message)
+        TextBrainRegion.exec(newArgs.toList, message)
       }
 
-      message.getChannel.sendMessageAsync(s"Tini will repeat `${arguments.drop(cmdStart).mkString(" ")}` " +
-        s"***${count.get}*** times and every ***${duration.getOrElse(defaultDuration) minutes}***",null)
+      message.getChannel.sendMessageAsync(s"Tini will repeat `${newArgs.mkString(" ")}` " +
+        s"***${count.get + 1}*** times and every ***${duration.getOrElse(minimumDuration) minutes}***" +
+        (if(isRescheduled) " (rescheduled to minimum duration)" else ""), null)
 
       repTask.runAsync
-      val tasks  = repeatTasks.getOrElseUpdate(message.getChannelId,new ListBuffer[CancelableFuture[Unit]])
-      val future = repTask.delayExecution(duration.getOrElse(defaultDuration) minutes)
+      val tasks  = repeatTasks.getOrElseUpdate(message.getChannelId, new ListBuffer[CancelableFuture[Unit]])
+      val future = repTask.delayExecution(duration.getOrElse(minimumDuration) minutes)
                           .restartUntil((Unit) => { count.getAndDecrement(1) == 1 } ) // because this gets the value before updating it
                           .runAsync
 
       future andThen { case _ => repeatTasks(message.getChannelId) -= future } // can't use onFinish from Task cuz variable is not fully set
       tasks += future
-    } else {
+    } else
       message.getChannel.sendMessageAsync("Usage: " + longHelp, null)
-    }
-
   }
 
   def stopRepeat(channelID: String): Unit = repeatTasks.get(channelID) match {
